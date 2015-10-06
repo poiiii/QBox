4f17a220b5dd324d30b956e2b5e93634f13260b1
poi~ <1448917547@qq.com>
2015-10-06 21:06:54 +0900

    Add token feedback

--- QBox/MainPage.xaml.cs
+++ QBox/MainPage.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿//Bug 1.保存文件时点取消会报异常= =
+//Bug 2.中文格式不对
+//仅支持下载= =
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -14,6 +17,7 @@ using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using Windows.Web.Http;
 using Windows.Storage;
+using System.Threading.Tasks;
 
 //“空白页”项模板在 http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409 上有介绍
 
@@ -30,28 +34,65 @@ namespace QBox
         }
 
         public StorageFile newfile { get; private set; }
+        //uri
+        public static string downcommond = "https://box.zjuqsc.com/item/get/";
+        public static string downcheck = "https://api.zjuqsc.com/box/get_api/";
 
         private async void Click_Here(object sender, RoutedEventArgs e)
         {
-            var downcommond = "https://box.zjuqsc.com/item/get/";
+            //BoxClient_api
             var token = textBox.Text;
             var boxclient = new HttpClient();
-            var response = await boxclient.GetAsync(new Uri(downcommond + token));
+            var response = await boxclient.GetAsync(new Uri(downcheck + token));
             var content = response.Content;
-            var filename = content.Headers.ContentDisposition.FileName;
-            var filebuffer = await content.ReadAsBufferAsync();
-            int dotpos = filename.LastIndexOf('.');
-            var extraname = filename.Substring(dotpos);
-            var savepicker = new Windows.Storage.Pickers.FileSavePicker();
-            savepicker.SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.DocumentsLibrary;
-            savepicker.FileTypeChoices.Add(new KeyValuePair<string, IList<string>>("file", new List<string> { extraname }));
-            savepicker.SuggestedFileName = filename;
-            newfile = await savepicker.PickSaveFileAsync();
-            var writestream = await newfile.OpenTransactedWriteAsync();
-            await writestream.Stream.WriteAsync(filebuffer);
-            await writestream.CommitAsync();
-            textBlock1.Text = "Your Download File Name is " + filename;
+            //Judge Err
+            var info = content.ToString();
+            int errpos = info.IndexOf("err");
+            var err = info.Substring(errpos + 5, 1);
+            switch (err)
+            {
+                case "2":
+                    {
+                        textBlock1.Text = "提取码不存在";
+                        break;
+                    }
+                case "1":
+                    {
+                        if (token.Length <= 15) textBlock1.Text = "提取码格式错误。必须仅包含字母、数字和下划线。";
+                        else textBlock1.Text = "提取码过长，最多15个字符。";
+                        break;
+                    }
+                case "0":
+                    {
+                        //get filename
+                        int filepos = info.IndexOf("filename");
+                        var filename = info.Substring(filepos + 11);
+                        int end = filename.IndexOf(",");
+                        filename = filename.Substring(0, end - 1);
+                        var extraname = filename.Substring(filename.LastIndexOf('.'));
 
+                        //BoxClient_getfile
+                        textBlock1.Text = "Your Downloading File Name is " + filename + "\nPlease Waiting...";
+                        response = await boxclient.GetAsync(new Uri(downcommond + token));
+                        content = response.Content;
+                        var filebuffer = await content.ReadAsBufferAsync();
+
+                        //Save File
+                        textBlock1.Text = "Your File Name is Downloaded\nChoose a Path to Save It";
+                        var savefile = new Windows.Storage.Pickers.FileSavePicker();
+                        savefile.SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.DocumentsLibrary;
+                        savefile.FileTypeChoices.Add(new KeyValuePair<string, IList<string>>("file", new List<string> { extraname }));
+                        savefile.SuggestedFileName = filename;
+                        newfile = await savefile.PickSaveFileAsync();
+                        var writefile = await newfile.OpenTransactedWriteAsync();
+                        await writefile.Stream.WriteAsync(filebuffer);
+                        await writefile.CommitAsync();
+                        //Fin.
+                        textBlock1.Text = "Your File is Saved";
+                        break;
+                    }
+            }
+            boxclient.Dispose();
         }
     }
 }
